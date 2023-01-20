868667509f3fb2d6fbc3e0e8ab8c20e3c77fdbf7
William O'Hanley <me@wohanley.com>
2023-01-20 15:53:22 -0800

    Single-chain matcher

--- interpreter/src/utils/match.js
+++ interpreter/src/utils/match.js
@@ -1,135 +1,142 @@
 const MATCH = "MATCH";
-const MISMATCH = "NOTMATCH";
+const MISMATCH = "MISMATCH";
 const PARTIAL_MATCH = "PARTIALMATCH";
 
 /**
- * Match a query against a program and return annotations.
- * @param {*} progam 
- * @param {*} query
+ * @param {*} program Parsed IPDL program
+ * @param {*} query An array of situations, ordered first cause to last effect
  * 
- * @return {
- *  inProgram: true | false,
- *  ambiguous: true | false,
- *  annotations : [],
- *  searchSpace: {}
- * }
+ * @returns {*} A tree of matches mirroring the structure of `program`
  */
-export default function match(program, query) {
-  let matchResult = {
-    inProgram: true,
-    ambiguous : true,
-    annotations : [...program.annotations],
-    searchSpace : {}
-  }
-
-  for (let chain of Object.values(program.chains)) {
-    runMatch(chain, query);
-  }
-
-
-  // run through the chain to find the first 'match'
-  function runMatch(fragment, query) {
-    
-    if (fragment.children) {
-      for (let item of fragment.children) {
-        let matchResult = runMatch(item, query);
-        if (matchResult.match === "MATCH") {
-
-        } 
-      }
-    }
-    else {
-      //does the first item of the query match?
-
-      let matchResult = itemMatch(fragment, query[0]);
-      if (!matchResult) {
-        console.log("Unhandled fragment", fragment);
-      }
-      return matchResult;
-    }
-
-  }
-
-
+export default function matchTree(program, query) {
+  return matchSection(program, query);
 }
 
-
-
-function itemMatch(item, queryItem) {
-    
-
-  // wildCard query always match 
-  if (queryItem.wildcard) {
-    return {match: MATCH};
+function matchSection(section, query) {
+  const instructions = [];
+  for (const instruction of Object.values(section.instructions || [])) {
+    instructions.push(matchInstruction(instruction, query));
+  }
+  
+  const sections = {};
+  for (const [name, subsection] of Object.entries(section.sections || {})) {
+    sections[name] = matchSection(subsection, query);
   }
 
-  if (item.type == "block") {
-
-    // does the item and query type match
-    if (!queryItem[item.name]) {
-      return {match: MISMATCH}
-    }
-
-    let deltaStack = [];
-    for (let testKey in queryItem[item.name]) {
-      // Property do not exist -> Mismatch
-      if (!item.properties[testKey]) {
-        return {match: MISMATCH}
-      }
-      const matchResult = propertyValueMatch(item.properties[testKey], queryItem[item.name][testKey]);
-      if (matchResult.match === MISMATCH) {
-        return {match: MISMATCH};
-      }
-        
-      // If propertyValueMatch can return MISMATCH we should handle it here
-    }
-
-    // We have a match, is it partial (run throug non defined in query obj keys)
-    let matchValue = {
-      match: MATCH
-    }
-    for (let propertyName in item.properties) {
-      if (item.properties[propertyName] != queryItem[item.name][propertyName]) {
-        matchValue.match == PARTIAL_MATCH;
-        if (! matchValue.delta) {
-          matchValue.delta = {};
-          matchValue.delta[item.name] = {}
-        }
-        matchValue.delta[item.name][propertyName] = item.properties[propertyName];
-      }
-    }
-
-    return matchValue;
-  }
-
+  return { instructions, sections };
 }
 
-function propertyValueMatch(propertyValue, queryValue) {
-
-  if (propertyValue.type == "enumValue") {
-    return (`${propertyValue.enumName}.${propertyValue.value}` == queryValue)? { match: MATCH} : { match: MISMATCH };
+function matchInstruction(instruction, query) {
+  // wildcard queries always match 
+  if (query.wildcard) {
+    return { match: MATCH, reason: 'wildcard' };
   }
 
-  if (propertyValue.type == "string") {
-    return (propertyValue.value == queryValue)? { match: MATCH} : { match: MISMATCH };
+  if (instruction.type === 'logic_block' || instruction.type === 'instruction') {
+    const children = instruction.children.map(i => matchInstruction(i, query));
+    return {
+      match: children.every(c => c.match === MATCH) ? MATCH : MISMATCH,
+      children
+    };
   }
 
-  if (propertyValue.type == "expression") {
+  if (instruction.operator === 'causal') {
+    if (!query.length) return {
+      match: MISMATCH,
+      reason: 'causal-not-array'
+    };
 
-    if (propertyValue.operator == "or") {
-
-      for (let item of propertyValue.children) {
-        const subResult = propertyValueMatch(item, queryValue);
-        if (subResult.match == MATCH) {
-          return subResult;
-        }
-      }
-      return {match: MISMATCH}
+    const children = [];
+    for (let i = 0; i < instruction.children.length; i++) {
+      // TODO this isn't right
+      if (i >= query.length) return { match: PARTIAL_MATCH, children };
+      children.push(matchInstruction(instruction.children[i], query[i]));
     }
 
-
+    return {
+      match: children.every(c => c.match === MATCH) ? MATCH : MISMATCH,
+      children
+    };
   }
-  console.warn("[MISIMPLEMENTATION MATCH] UNHANDLE PROPERTY VALUE", propertyValue); 
- 
-  return { match: MISMATCH};
+
+  if (instruction.type === 'block' || instruction.type === 'object') {
+    if (!query.properties) return {
+      match: MISMATCH,
+      reason: 'object-no-properties'
+    };
+
+    const properties = {};
+    for (const [name, pattern] of Object.entries(instruction.properties)) {
+      if (!query.properties[name]) {
+        properties[name] = {
+          match: MISMATCH,
+          reason: 'missing-property'
+        };
+      } else {
+        properties[name] = matchInstruction(pattern, query.properties[name]);
+      }
+    }
+
+    const match = Object.values(properties).every(p => p.match === MATCH);
+
+    return {
+      match: match ? MATCH : MISMATCH,
+      properties
+    };
+  }
+
+  if (instruction.type === 'string') {
+    if (query === instruction.value) return { match: MATCH };
+    return {
+      match: MISMATCH,
+      reason: 'string-mismatch',
+      expected: instruction.value,
+      actual: query
+    };
+  }
+
+  if (instruction.type === 'variable') {
+    // TODO
+    return { match: MATCH };
+  }
+
+  if (instruction.operator === 'and') {
+    const children = instruction.children.map(i => matchInstruction(i, query));
+    return {
+      match: children.every(r => r.match === MATCH) ? MATCH : MISMATCH,
+      children
+    };
+  }
+
+  if (instruction.operator === 'or') {
+    const children = instruction.children.map(i => matchInstruction(i, query));
+    return {
+      match: children.some(r => r.match === MATCH) ? MATCH : MISMATCH,
+      children
+    };
+  }
+
+  if (Array.isArray(instruction)) {
+    if (!Array.isArray(query)) return {
+      match: MISMATCH,
+      reason: 'non-array'
+    };
+
+    const children = [];
+    for (let i = 0; i < instruction.length; i++) {
+      if (i >= query.length) return {
+        match: MISMATCH,
+        reason: 'array-too-short',
+        children
+      };
+      children.push(matchInstruction(instruction[i], query[i]));
+    }
+
+    return {
+      match: children.every(r => r.match === MATCH) ? MATCH : MISMATCH,
+      children
+    };
+  }
+
+  throw new Error(`Unknown instruction type: ${JSON.stringify(instruction)}`);
 }
--- interpreter/testMatch.js
+++ interpreter/testMatch.js
@@ -1,23 +1,49 @@
-
 import parser from './src/parser/parser.js';
+import matchTree from './src/utils/match.js';
 
-import match from './src/utils/match.js';
+const program = parser('sample/weddingInsuranceSectioned/weddingInsuranceSectioned.ipdl');
 
-let program = parser(process.argv[2]);
-
-// query is a causal chain as an array
-// empty
-match(program,
-  [
-    {
-      Situation: {
-      event: "Event.NoShow"
-      }
-    },
-    {
-      Situation: {
-        event: "Event.UnavoidableCancellation"
+matchTree(program, [{
+  name: "Situation",
+  properties: {
+    event: "Event.NoShow",
+    relation: {
+      properties: {
+        type: "Relation.CausedBy",
+        target: "intended officiating minister or registrar"
       }
     }
-  ]
-);
+  }
+}, {
+  name: "Situation",
+  properties: {
+    event: "Event.UnavoidableCancellation",
+    relation: [{
+      properties: {
+        type: "Relation.HappenTo",
+        target: "wedding"
+      }
+    }, {
+      properties: {
+        type: "Relation.CausedBy",
+        target: "me"
+      }
+    }]
+  }
+}, {
+  name: "Situation",
+  properties: {
+    event: "Event.IntentionToReplan",
+    relation: [{
+      properties: {
+        type: "Relation.HappenTo",
+        target: "wedding"
+      }
+    }, {
+      properties: {
+        type: "Relation.By",
+        target: "me"
+      }
+    }]
+  }
+}]);
